e3034c97e7cecb3df82d43510a9ffd3adba1c89a
Isaac Mackle <isaac.mackle@gmail.com>
2024-10-03 15:46:20 +1300

    feat: allow format disable

--- lua/lsp/plugins/autoformat.lua
+++ lua/lsp/plugins/autoformat.lua
@@ -6,6 +6,24 @@ local jsFmt = function(bufnr)
   -- end
 end
 
+vim.api.nvim_create_user_command('FormatDisable', function(args)
+  if args.bang then
+    -- FormatDisable! will disable formatting just for this buffer
+    vim.b.disable_autoformat = true
+  else
+    vim.g.disable_autoformat = true
+  end
+end, {
+  desc = 'Disable autoformat-on-save',
+  bang = true,
+})
+vim.api.nvim_create_user_command('FormatEnable', function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+end, {
+  desc = 'Re-enable autoformat-on-save',
+})
+
 return {
   'stevearc/conform.nvim',
   lazy = false,
@@ -25,6 +43,9 @@ return {
       -- Disable "format_on_save lsp_fallback" for languages that don't
       -- have a well standardized coding style. You can add additional
       -- languages here or re-enable it for the disabled ones.
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
+      end
       local disable_filetypes = { c = true, cpp = true }
       return {
         timeout_ms = 500,
